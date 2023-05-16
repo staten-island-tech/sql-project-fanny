919fea0f852cadc8b524700aebc50c56931ecd34
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-05-16 10:13:16 -0400

    add pinia

--- sql-project/src/main.js
+++ sql-project/src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(createPinia())
-
-app.mount('#app')
+app.use(createPinia());
+app.use(pinia);
+app.mount("#app");
