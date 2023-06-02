ab1c97c74ae65cc091f59f6bfd24336678aa4905
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-06-02 10:43:39 -0400

    create a view of all the tables

---
+++ sql-project/src/components/TablesView.vue
@@ -0,0 +1,4 @@
+create view transcripts as select date.datedisplay, students.type,
+courses.title, courses.code, grades.result from grades left join students on
+grades.student_id = students.id left join courses on grades.course_id =
+courses.id; alter view transcripts owner to authenticated;
