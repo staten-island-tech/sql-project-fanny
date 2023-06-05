24784fe606280df30874d8f1b692f7dea46172e3
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-06-05 09:59:18 -0400

    added buttons

---
+++ sql-project/src/stores/buttons.js
@@ -0,0 +1,17 @@
+import { Button } from "@supabase/ui";
+
+export default function ButtonTypes() {
+  return (
+    <>
+      <Button type="default">Default Button</Button>
+    </>
+  );
+}
+
+export default function ButtonSizes() {
+    return (
+      <>
+        <Button size="medium">medium</Button>
+      </>
+    );
+  }
